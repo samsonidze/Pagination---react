5d5c98386ea1db8925e849d9e8cadb7aae7d905c
Giorgi Samsonidze <samsonidze80@gmail.com>
2026-04-25 21:39:47 +0400

    pagination

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Posts from "./Posts";
+import Pagination from "./Pagenation";
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  // get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  const paginate = (pageNumbers) => setCurrentPage(pageNumbers);
+
+  return (
+    <div className="container">
+      <h1 className="text_primary"> My Blog</h1>
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Posts.jsx
@@ -0,0 +1,13 @@
+export default function Posts({ posts, loading }) {
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <ul className="list-group">
+      {posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  );
+}
